cb3a96e549dccfbaa6d0e61e95e9c7339419014d
mrexojo <mrexojo@gmail.com>
2017-11-19 13:06:02 +0100

    lookup data instance

--- 1-t2/output.tf
+++ 1-t2/output.tf
@@ -1,3 +1,15 @@
 output "public_ip" {
  value = "${aws_instance.demo1_terraform.public_ip}"
 }
+output "private_ip" {
+ value = "${aws_instance.demo1_terraform.private_ip}"
+}
+output "public_dns" {
+ value = "${aws_instance.demo1_terraform.public_dns}"
+}
+output "availability_zone" {
+ value = "${aws_instance.demo1_terraform.availability_zone}"
+}
+output "volume_size" {
+ value = "${aws_instance.demo1_terraform.volume_size}"
+}
